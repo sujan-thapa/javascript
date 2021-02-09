706dec037ff6045d734b6d6538bc38de10204454
Sujan Thapa <sujanthapamagar960@gmail.com>
2021-02-09 22:03:58 +0545

    eventObjects

--- js/tut17.js
+++ js/tut17.js
@@ -1,1 +1,18 @@
 console.log('Welcome to the seventeen');
+
+
+document.getElementById('heading').addEventListener('click',function(e){
+    let variable;
+
+
+    console.log('you clicked heading');
+    // location.href = '//twitter.com'
+    variable = e.target;
+    variable = e.target.className;
+    variable = Array.from(e.target.classList);
+    variable = e.offsetX;
+    variable = e.offsetY;
+    variable = e.clientX;
+    // variable = e.clientY;
+    console.log(variable);
+})
